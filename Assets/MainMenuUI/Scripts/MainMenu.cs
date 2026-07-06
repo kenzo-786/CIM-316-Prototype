5bf32fc9af534815f0f0dd9810803906233b425c
Jeffrey L. Salvino II <jeffrey.salvino@student.saedubai.com>
2026-07-07 01:36:48 +0400

    Fixed the play button in the mainmenu

--- Assets/MainMenuUI/Scripts/MainMenu.cs
+++ Assets/MainMenuUI/Scripts/MainMenu.cs
@@ -16,7 +16,7 @@ public class MainMenu : MonoBehaviour
     
     public void PlayGame()
     {
-        SceneManager.LoadScene(gameScene);
+        SceneManager.LoadScene(1);
     }
 
     public void OpenCredits()
